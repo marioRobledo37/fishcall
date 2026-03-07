a2c9f0ffa4a19d19f616c33a7215506dfd012c7e
Mario Robledo <mariorobledomkt@gmail.com>
2026-03-06 23:08:13 -0300

    create admin automatically

--- pesca_backend/config/wsgi.py
+++ pesca_backend/config/wsgi.py
@@ -7,10 +7,28 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
+from django.contrib.auth.models import User
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@fishcall.com",
+        password="admin123"
+    )
