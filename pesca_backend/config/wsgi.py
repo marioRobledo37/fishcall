078666ce7655b76d64cccb798fe2f897f2878a10
Mario Robledo <mariorobledomkt@gmail.com>
2026-03-07 11:19:02 -0300

    revision 2

--- pesca_backend/config/wsgi.py
+++ pesca_backend/config/wsgi.py
@@ -1,26 +1,11 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
-from django.contrib.auth.models import User
-
+import os
 from django.core.wsgi import get_wsgi_application
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
 
-    
-import os
 import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
@@ -29,7 +14,7 @@ User = get_user_model()
 
 if not User.objects.filter(username="admin").exists():
     User.objects.create_superuser(
-        username="admin",
-        email="admin@fishcall.com",
-        password="admin123"
+        "admin",
+        "admin@fishcall.com",
+        "admin123"
     )
