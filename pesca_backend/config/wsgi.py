39ff794da239379e1b9e79a1943aa64a9de23c83
Mario Robledo <mariorobledomkt@gmail.com>
2026-03-07 11:26:04 -0300

    fix wsgi

--- pesca_backend/config/wsgi.py
+++ pesca_backend/config/wsgi.py
@@ -1,20 +1,7 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-
-import django
-django.setup()
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        "admin",
-        "admin@fishcall.com",
-        "admin123"
-    )
